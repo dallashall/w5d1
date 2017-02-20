3f436c7a3917ff148bf58a3aabc5e7dfb9689cd5
App Academy Student <student@appacademy.io>
2017-02-20 11:48:25 -0800

    add user model tests and model and user conroller specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of(:username)}
+    it {should validate_presence_of(:password_digest)}
+    it {should validate_presence_of(:session_token)}
+    it {should validate_length_of(:password).is_at_least(6)}
+  end
+
+  describe 'associations' do
+    it {should have_many(:goals)}
+    it {should have_many(:cheers)}
+    it {should have_many(:comments)}
+  end
+
+  describe 'class methods' do
+    before(:each) { user = User.create(username: 'Leo', password: '123456')}
+    it "finds by credentials" do
+      expect(User.find_by_credentials(username: 'Leo', password: '123456')).to be_instance_of(User)
+      expect(User.find_by_credentials(username: 'Leo', password: '123456').username).to eq('Leo')
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,19 +29,6 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    # Choose a test framework:
-    with.test_framework :rspec
-
-    # # Choose one or more libraries:
-    # with.library :active_record
-    # with.library :active_model
-    # with.library :action_controller
-    # Or, choose the following (which implies all of the above):
-    with.library :rails
-  end
-end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -67,4 +54,17 @@ end
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+
+    # # Choose one or more libraries:
+    # with.library :active_record
+    # with.library :active_model
+    # with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
 end
